316b847f60dca40b2044eed2646064470d473ccc
Kathryn Nanz <nanz@mcwfhnanz.attlocal.net>
2021-09-02 16:24:29 -0400

    small test commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World
+          Hello World, I'm Kathryn
         </a>
       </header>
     </div>
