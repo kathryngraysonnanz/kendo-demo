8f805adb2e12ef4214f66e565ca684c168951ee1
Kathryn Nanz <kathrynkaydesigns@gmail.com>
2022-07-25 10:48:09 -0400

    removed old line from pre-React-18 rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createRoot } from 'react-dom/client';
 import './index.scss';
 import './styles/LCARS.css'
 import App from './app/router';
